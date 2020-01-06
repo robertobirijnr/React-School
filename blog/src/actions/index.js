99bba68949e3d6ce1aa471c60043fcebd9cd45d2
robertobirijnr <robertobirijnr@hotmail.com>
2020-01-06 11:59:36 +0000

    some changes

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import jsonplaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async dispatch => {
@@ -6,8 +7,18 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => async dispatch =>{
-  const response = await jsonplaceholder.get(`users/${id}`)
+// export const fetchUser = id => async dispatch => {
+//   const response = await jsonplaceholder.get(`users/${id}`);
 
-  dispatch({type:'FETCH_USER',payload:response.data})
-}
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// };
+
+export const fetchUser = id => dispatch => {
+  _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonplaceholder.get(`users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
+});
